8ded42b586c653084b2d088be8e1bb5deaaf79d3
Ankita Nigam <ankitanigam51@gmail.com>
2020-11-19 12:02:20 +0530

    Working proper

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -9,8 +9,54 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    updateXY(0,0);
+  };
+
+  const start = () => {
+    setRenderBall(true);
+  }
+  const renderChoice = () => {
+    return renderBall ? 
+    (<div className="ball" style={{
+      position: 'absolute',
+      left: ballPosition.left,
+      top: ballPosition.top
+    }}></div>
+    ) :(
+    <button onClick={start} className="start">
+      Start</button>);
+  };
+
+ const updateXY = (newX,newY) => {
+   setX(newX);
+   setY(newY);
+   setBallPosition({
+     left: newX + 'px',
+     top: newY + 'px'
+   });
+ }
+
+  useEffect(() => {
+    const kListener = (evt) => {
+      console.log("Required key is Pressed");
+      if(renderBall) {
+        if(evt.keyCode === 37){
+          updateXY(x-5, y);
+        }else if(evt.keyCode === 38){
+          updateXY(x, y-5);
+        }else if(evt.keyCode === 39){
+          updateXY(x+5, y);
+        }else if(evt.keyCode === 40){
+          updateXY(x, y+5);
+        }
+      }
+    };
+    document.addEventListener("keydown", kListener);
+    //cleanup
+    return () => document.removeEventListener("keydown", kListener);
+  })
 
   return (
     <div className="playground">
